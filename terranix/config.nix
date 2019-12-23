3e781db542b5573c0156da2aa3cbe38760469f38
Ingolf Wagner <contact@ingolf-wagner.de>
2019-12-23 15:03:30 +1300

    recovered terranix/config.nix

--- terranix/config.nix
+++ terranix/config.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ...}:
 let
   hcloud-modules = pkgs.fetchgit {
-    url = "https://git.ingolf-wagner.de/terranix/hcloud.git";
+    url = "https://github.com/mrVanDalo/terranix-hcloud.git";
     rev = "b6896f385f45ecfd66e970663c55635c9fd8b26b";
     sha256 = "1bggnbry7is7b7cjl63q6r5wg9pqz0jn8i3nnc4rqixp0ckwdn85";
   };
@@ -10,7 +10,22 @@ in
 
   imports = [
     (toString hcloud-modules)
+
+    ./config/file-generation.nix
+    ./config/ssh-setup.nix
   ];
 
+  hcloud.nixserver.lineageOS-builder = {
+    serverType = "ccx31";
+    configurationFile = pkgs.writeText "configuration.nix" ''
+      { pkgs, lib, ... }:
+      {
+        environment.systemPackages = with pkgs; [
+          htop git vim mosh
+        ];
+        networking.firewall.allowedUDPPorts = [ 60001 ];
+      }
+      '';
+  };
 
 }
